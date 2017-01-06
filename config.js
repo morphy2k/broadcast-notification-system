ac9cda074adb3bc823656f1f47b52c6314962f87
Markus Wiegand <markus.wiegand@outlook.com>
2017-01-06 01:52:46 +0100

    beautify changes

--- config.js
+++ config.js
@@ -3,10 +3,10 @@
 // # main configuration
 
 module.exports = {
-    server: {
-        url: 'http://localhost:8083', // if you use a special port like 'http://example.com:8080', please add it!
-        port: '8083',
-        type: 0, // '0' = normal, '1' = proxy
-        host: 'localhost' // only for 'proxy' type
-    }
+	server: {
+		url: 'http://localhost:8083', // if you use a special port like 'http://example.com:8080', please add it!
+		port: '8083',
+		type: 0, // '0' = normal, '1' = proxy
+		host: 'localhost' // only for 'proxy' type
+	}
 };
